ec9a0b00509da796476dee2d9032606da9e9292e
Yasu <yassantune@gmail.com>
2018-05-22 05:35:45 +0000

    added game files

---
+++ w7/two-o-player/main.rb
@@ -0,0 +1,13 @@
+require './game.rb'
+
+game = Game.new
+
+puts "=================================="
+puts "Welcome to Two-O-Player Math Game!"
+puts "=================================="
+
+game.run
+
+puts "=============================="
+puts "Thanks for playing! (๑ʻัદʻั๑);"
+puts "=============================="
